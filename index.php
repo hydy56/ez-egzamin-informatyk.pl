d5235d3c5042e31e7fd02e98cf6fc50f233679e4
hydy56 <cukeirkwoymati@gmail.com>
2024-12-08 23:24:50 +0100

    Fix input

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <form action="odpowiedzi-inf03-ee09-programowanie-bazy-danych.php" method="POST">
-        <input type="text" id="odp" name="odp" placeholder="Ile odpowiedzi" required />
+        <input type="number" style="width:100px;" id="odp" name="odp" placeholder="Ile odpowiedzi" max="40" min="0"required />
         <button type="submit" >Wyślij</button>
     </form>
 </body>
