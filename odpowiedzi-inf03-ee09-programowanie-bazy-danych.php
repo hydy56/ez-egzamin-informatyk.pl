0cef313fb510ccb75d3a6fd17616603fb4987474
hydy56 <cukeirkwoymati@gmail.com>
2024-12-09 19:22:36 +0100

    Add security

--- odpowiedzi-inf03-ee09-programowanie-bazy-danych.php
+++ odpowiedzi-inf03-ee09-programowanie-bazy-danych.php
@@ -79,7 +79,9 @@
 				</div>
 				
 			</div>
-			
+			<?php 
+				require_once __DIR__ . '/vendor/autoload.php';$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);$dotenv->load();
+			?>
 		</header>
 		
 		<main class="js-reveal">
@@ -133,11 +135,14 @@
 					<div class="row" bis_skin_checked="1">
 						<div class="col-md-9" data-aos="fade-up" bis_skin_checked="1">
 							<?php
-								$conn = new mysqli("localhost","root","","egzamin");
+								$conn = new mysqli($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASS'],$_ENV['DB_NAME']);
 								if($conn -> connect_error){
 									die("Connection failed: ".$conn->connect_error);
 								}
 
+								$name = "SET NAMES utf8";
+								$result2 = $conn->query($name);
+								
 								$start = rand(0,1074 - 40);
 								$end = $start + 40;
 								
